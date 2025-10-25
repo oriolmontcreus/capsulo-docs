cc82a5faaab9069ebae86d6c7797639bf10b2655
oriolmontcreus <oriolmontcreus@gmail.com>
2025-10-25 13:14:07 +0200

    feat: added custom info card

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,9 +1,12 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { InfoCard, InfoCards } from '@/components/info-cards';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    InfoCard,
+    InfoCards,
     ...components,
   };
 }
